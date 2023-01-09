83910b9874006f5c7518783498c9d0a1671915b6
Amorzim <tamilacambe@gmail.com>
2023-01-09 17:44:28 -0300

    First-function-and-api
    show warning function created and API added

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ document.querySelector('.busca').addEventListener('submit', (e) => {
     let input = document.querySelector('#searchInput').value;
 
     if (input !== '') {
-        showWarning('Carregando')
+        showWarning('Carregando...');
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encondeURI(input)}&appid={API key}`;
     }
 
 });
