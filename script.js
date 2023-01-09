22d2fedc849c590023ff6635390281854b68fcc6
Amorzim <tamilacambe@gmail.com>
2023-01-09 18:16:23 -0300

    image taken

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('.busca').addEventListener('submit', (e) => {
+document.querySelector('.busca').addEventListener('submit', async(e) => {
     e.preventDefault();
 
     let input = document.querySelector('#searchInput').value;
@@ -6,10 +6,32 @@ document.querySelector('.busca').addEventListener('submit', (e) => {
     if (input !== '') {
         showWarning('Carregando...');
 
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encondeURI(input)}&appid={API key}`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=820fa4c7fe42d71f764bd0bb8b59512f&units=metric&lang=pt_br`;
+
+        let results = await fetch(url);
+        let json = await results.json();
+
+        if (json.cod === 200) {
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpead: json.wind.speed,
+                windAngle: json.wind.deg
+            });
+        } else {
+            showWarning('Não encontramos esta localização.');
+        }
     }
 
 });
+
+function showInfo(json) {
+    showWarning('');
+
+    document.querySelector('.resultado').style.display = 'block';
+}
 
 function showWarning(msg) {
     document.querySelector('.aviso').innerHTML = msg
