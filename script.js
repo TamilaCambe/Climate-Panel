b35887cb75737bf1c7ec8f76f3f12f1440c7e6f5
Amorzim <tamilacambe@gmail.com>
2023-01-09 17:33:44 -0300

    height and width erased

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-document.querySelector('.busca').addEventListener('submit', (event) => {
-    event.preventDefault();
+document.querySelector('.busca').addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    let input = document.querySelector('#searchInput').value
+    let input = document.querySelector('#searchInput').value;
 
     if (input !== '') {
-
+        showWarning('Carregando')
     }
 
 });
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg
+}
